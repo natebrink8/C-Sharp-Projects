f50661a967e5c32bb62f54ad397bd915e06ba25c
nate <147548341+natebrink8@users.noreply.github.com>
2024-03-27 06:27:49 -0700

    LMS Step 200 code push.

---
+++ Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your age?");         // Display the input request.
+        string age = Console.ReadLine();                // Create variable to read the string input.
+        int age1 = Convert.ToInt32(age);                // Create integer variable, converting from the string variable.
+
+        Console.WriteLine("Have you ever had a DUI? (Please only True or False.)");  // Display the input request.
+        string dui = Console.ReadLine();                // Create variable to read the string input.
+        bool dui1 = Convert.ToBoolean(dui);
+
+        Console.WriteLine("How many speeding tickets do you have?");    // Display the input request.
+        string tickets = Console.ReadLine();                            // Create variable to read the string input.
+        int tickets1 = Convert.ToInt32(tickets);
+
+        bool qualified = (age1 > 15 && dui1 == false && tickets1 < 3);
+        Console.WriteLine("Qualified: " + qualified);
+
+        Console.ReadLine();
+    }
+}
+
