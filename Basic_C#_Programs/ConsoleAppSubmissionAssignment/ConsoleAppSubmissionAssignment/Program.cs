3bc9747e3e4c5170ecd0051abe9dc8a2efa4d8a7
nate <147548341+natebrink8@users.noreply.github.com>
2024-03-27 14:47:31 -0700

    committing code for the CONSOLE APP SUBMISSION ASSIGNMENT.

---
+++ Basic_C#_Programs/ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Guess my favorite color.");
+        string color = Console.ReadLine();
+        bool colorGuess = color == "blue";
+
+        do
+        {
+            switch (color)
+            {
+                case "red":                                                 // Case for other colors.
+                    Console.WriteLine("You said red. Go back to bed.");     // Console display text to keep 'em coming!
+                    Console.WriteLine("Guess again.");                      // (Lather, Rinse, Repeat in below case blocks.)
+                    color = Console.ReadLine();
+                    break;
+                case "yellow":
+                    Console.WriteLine("Yellow is not for this fellow.");
+                    Console.WriteLine("Guess again.");
+                    color = Console.ReadLine();
+                    break;
+                case "green":
+                    Console.WriteLine("Green is clean, ya know what I mean? But I'm not keen.");
+                    Console.WriteLine("Guess again.");
+                    color = Console.ReadLine();
+                    break;
+                case "purple":
+                    Console.WriteLine("Purple is a \"No\" for me.");
+                    Console.WriteLine("Guess again.");
+                    color = Console.ReadLine();
+                    break;
+                case "orange":
+                    Console.WriteLine("Orange is a juice, you silly goose!");
+                    Console.WriteLine("Guess again.");
+                    color = Console.ReadLine();
+                    break;
+                case "blue":                                                // Winner! Winner! Chicken dinner!
+                    Console.WriteLine("Look at you, guessing blue. This much is true... my favorite color is blue!");
+                    colorGuess = true;                                      // Set the colorGuess var to true to end this loop.
+                    break;
+                default:                                                    // Default catch-all for all other responses entered in console.
+                    Console.WriteLine("My liege... My Lord... What!!  Incorrect.");
+                    Console.WriteLine("Guess another color, please.");
+                    color = Console.ReadLine();
+                    break;
+            }
+        }
+        while (!colorGuess);        // While the colorGuess variable is false (see top: favorite color = blue).
+
+        Console.ReadLine();         // Read 
+    }
+}
--- Basic_C#_Programs/LoopsVideoETC/LoopsVideoETC/Program.cs
+++ Basic_C#_Programs/LoopsVideoETC/LoopsVideoETC/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 
 class Program
 {
@@ -27,11 +22,6 @@ class Program
                     Console.WriteLine("Guess a number.");
                     number = Convert.ToInt32(Console.ReadLine());
                     break;
-                case 55:
-                    Console.WriteLine("You guessed 55. Try again.");
-                    Console.WriteLine("Guess a number.");
-                    number = Convert.ToInt32(Console.ReadLine());
-                    break;
                 case 12:
                     Console.WriteLine("You guessed 12, that is correct!");
                     isGuessed = true;
@@ -48,3 +38,47 @@ class Program
         Console.ReadLine();
     }
 }
+
+
+
+
+
+// LOOP code for While/Do
+// //////////////////////
+//Console.WriteLine("Guess a number.");
+//int number = Convert.ToInt32(Console.ReadLine());
+//bool isGuessed = number == 12;
+
+//do
+//{
+//    switch (number)
+//    {
+//        case 62:
+//            Console.WriteLine("You guessed 62. Try again.");
+//            Console.WriteLine("Guess a number.");
+//            number = Convert.ToInt32(Console.ReadLine());
+//            break;
+//        case 29:
+//            Console.WriteLine("You guessed 29. Try again.");
+//            Console.WriteLine("Guess a number.");
+//            number = Convert.ToInt32(Console.ReadLine());
+//            break;
+//        case 55:
+//            Console.WriteLine("You guessed 55. Try again.");
+//            Console.WriteLine("Guess a number.");
+//            number = Convert.ToInt32(Console.ReadLine());
+//            break;
+//        case 12:
+//            Console.WriteLine("You guessed 12, that is correct!");
+//            isGuessed = true;
+//            break;
+//        default:
+//            Console.WriteLine("You are wrong.");
+//            Console.WriteLine("Guess a number.");
+//            number = Convert.ToInt32(Console.ReadLine());
+//            break;
+//    }
+//}
+//while (!isGuessed);
+
+//Console.ReadLine();
