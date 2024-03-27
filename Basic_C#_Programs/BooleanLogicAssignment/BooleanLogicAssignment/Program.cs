4b969c81c98fd8639af6629b9fd7b00a7ff990c8
nate <147548341+natebrink8@users.noreply.github.com>
2024-03-27 06:32:14 -0700

    Take 3.

--- Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
+++ Basic_C#_Programs/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -10,14 +10,14 @@ class Program
 
         Console.WriteLine("Have you ever had a DUI? (Please only True or False.)");  // Display the input request.
         string dui = Console.ReadLine();                // Create variable to read the string input.
-        bool dui1 = Convert.ToBoolean(dui);
+        bool dui1 = Convert.ToBoolean(dui);             // Create boolean variable, converting from the string variable.
 
         Console.WriteLine("How many speeding tickets do you have?");    // Display the input request.
-        string tickets = Console.ReadLine();                            // Create variable to read the string input.
-        int tickets1 = Convert.ToInt32(tickets);
+        string tickets = Console.ReadLine();            // Create variable to read the string input.
+        int tickets1 = Convert.ToInt32(tickets);        // Create integer variable, converting from the string variable.
 
-        bool qualified = (age1 > 15 && dui1 == false && tickets1 < 3);
-        Console.WriteLine("Qualified: " + qualified);
+        bool qualified = (age1 > 15 && dui1 == false && tickets1 < 3);  // Boolean variable equals above 3 variables AND AND AND check.
+        Console.WriteLine("Qualified: " + qualified);   // Display true or false, if qualified.
 
         Console.ReadLine();
     }
