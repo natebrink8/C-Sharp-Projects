fda03ac65139f6aeb6d983520f1036154def9ad3
nate <147548341+natebrink8@users.noreply.github.com>
2024-03-27 11:13:05 -0700

    Pushing top folder for assignment.

--- Basic_C#_Programs/DailyReportSubmission/DailyReportSubmission/Program.cs
+++ Basic_C#_Programs/DailyReportSubmission/DailyReportSubmission/Program.cs
@@ -15,19 +15,19 @@ class Program
         Console.ReadLine();                                 // Read the course name.
 
         Console.WriteLine("What page number?");             // Ask student what page number they are on.
-        Console.ReadLine();                                 // Read the page number.
+        int pageNumber = Convert.ToInt32(Console.ReadLine());   // Read the pageNumber variable with data type int.
 
         Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false.\"");      // Ask student if they need help.
-        Console.ReadLine();                                 // Read the help response.
+        bool help = Convert.ToInt32(Console.ReadLine());     // Read the help variable with data type boolean.
 
         Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");       // Ask student for positive experiences.
         Console.ReadLine();                                 // Read the positive variable with data type string.
 
-        Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");                // Ask student for feedback.
+        Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");    // Ask student for feedback.
         Console.ReadLine();                                 // Read the feedback variable with data type string.
 
         Console.WriteLine("How many hours did you study today?");       // Ask student how many hours they studied.
-        Console.ReadLine();                                 // Read the hoursToday variable with data type int.
+        int studyHours = Convert.ToInt32(Console.ReadLine());           // Read the studyHours variable with data type int.
 
         Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");     // SAY THANK YOU!
         Console.ReadLine();                                 // Display THANK YOU! ;-)
