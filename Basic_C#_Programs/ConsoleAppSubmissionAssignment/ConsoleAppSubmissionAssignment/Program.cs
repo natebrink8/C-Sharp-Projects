10f51cbd34a7f9d419270cb54c9c6b9c04aeae2e
nate <147548341+natebrink8@users.noreply.github.com>
2024-03-27 17:22:05 -0700

    updated to add while statement for the CONSOLE APP SUBMISSION ASSIGNMENT.

--- Basic_C#_Programs/ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
+++ Basic_C#_Programs/ConsoleAppSubmissionAssignment/ConsoleAppSubmissionAssignment/Program.cs
@@ -1,56 +1,69 @@
 ﻿using System;
 
-
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Guess my favorite color.");
+        Console.WriteLine("You have 5 guesses at my favorite color.");
         string color = Console.ReadLine();
         bool colorGuess = color == "blue";
 
-        do
+        int initialColorGuessTokens = 4;
+        int colorGuessBalance = initialColorGuessTokens;
+        int colorGuessToken = 1;
+                
+        while (colorGuessBalance > 0)
         {
-            switch (color)
+            Console.WriteLine("Guesses left: " + colorGuessBalance);
+            colorGuessBalance -= colorGuessToken;
+
+            do
             {
-                case "red":                                                 // Case for other colors.
-                    Console.WriteLine("You said red. Go back to bed.");     // Console display text to keep 'em coming!
-                    Console.WriteLine("Guess again.");                      // (Lather, Rinse, Repeat in below case blocks.)
-                    color = Console.ReadLine();
-                    break;
-                case "yellow":
-                    Console.WriteLine("Yellow is not for this fellow.");
-                    Console.WriteLine("Guess again.");
-                    color = Console.ReadLine();
-                    break;
-                case "green":
-                    Console.WriteLine("Green is clean, ya know what I mean? But I'm not keen.");
-                    Console.WriteLine("Guess again.");
-                    color = Console.ReadLine();
-                    break;
-                case "purple":
-                    Console.WriteLine("Purple is a \"No\" for me.");
-                    Console.WriteLine("Guess again.");
-                    color = Console.ReadLine();
-                    break;
-                case "orange":
-                    Console.WriteLine("Orange is a juice, you silly goose!");
-                    Console.WriteLine("Guess again.");
-                    color = Console.ReadLine();
-                    break;
-                case "blue":                                                // Winner! Winner! Chicken dinner!
-                    Console.WriteLine("Look at you, guessing blue. This much is true... my favorite color is blue!");
-                    colorGuess = true;                                      // Set the colorGuess var to true to end this loop.
-                    break;
-                default:                                                    // Default catch-all for all other responses entered in console.
-                    Console.WriteLine("My liege... My Lord... What!!  Incorrect.");
-                    Console.WriteLine("Guess another color, please.");
-                    color = Console.ReadLine();
-                    break;
+                switch (color)
+                {
+                    case "red":                                                 // Case for other colors.
+                        Console.WriteLine("You said red. Go back to bed.");     // Console display text to keep 'em coming!
+                        Console.WriteLine("Guess again.");                      // (Lather, Rinse, Repeat in below case blocks.)
+                        color = Console.ReadLine();
+                        break;
+                    case "yellow":
+                        Console.WriteLine("Yellow is not for this fellow.");
+                        Console.WriteLine("Guess again.");
+                        color = Console.ReadLine();
+                        break;
+                    case "green":
+                        Console.WriteLine("Green is clean, ya know what I mean? But I'm not keen.");
+                        Console.WriteLine("Guess again.");
+                        color = Console.ReadLine();
+                        break;
+                    case "purple":
+                        Console.WriteLine("Purple is a \"No\" for me.");
+                        Console.WriteLine("Guess again.");
+                        color = Console.ReadLine();
+                        break;
+                    case "orange":
+                        Console.WriteLine("Orange is a juice, you silly goose!");
+                        Console.WriteLine("Guess again.");
+                        color = Console.ReadLine();
+                        break;
+                    case "blue":                                                // Winner! Winner! Chicken dinner!
+                        Console.WriteLine("Look at you, guessing blue. This much is true... my favorite color is blue!");
+                        colorGuess = true;                                      // Set the colorGuess var to true to end this loop.
+                        break;
+                    default:                                                    // Default catch-all for all other responses entered in console.
+                        Console.WriteLine("My liege... My Lord... What!!  Incorrect.");
+                        Console.WriteLine("Guess another color, please.");
+                        color = Console.ReadLine();
+                        break;
+                }
+
+                Console.WriteLine("Guesses left: " + colorGuessBalance);        // Proper placement for updating count.
+                colorGuessBalance -= colorGuessToken;
             }
+
+            while (!colorGuess && colorGuessBalance > 0);       // While the colorGuess variable is false (see top: favorite color = blue).
+                                                                // AND While the colorGuessBalance is greater than zero.
         }
-        while (!colorGuess);        // While the colorGuess variable is false (see top: favorite color = blue).
 
         Console.ReadLine();         // Read 
     }
---
+++ Basic_C#_Programs/StringFuncVideo/StringFuncVideo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Nate";
+            string quote = "The man said, \"Hello\", " + name + "." + "\nHello on a new line. \n \t Hello on a tab.";
+
+        Console.WriteLine(quote);
+        
+        
+        Console.ReadLine();
+        }
+    }
