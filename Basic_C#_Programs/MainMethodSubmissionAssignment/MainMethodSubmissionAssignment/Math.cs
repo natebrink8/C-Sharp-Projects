2d48e5940e7b92bc80cbc5977b0502bf3540fe84
nate <147548341+natebrink8@users.noreply.github.com>
2024-03-29 13:19:11 -0700

    MAIN METHOD SUBMISSION ASSIGNMENT code commit.

---
+++ Basic_C#_Programs/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Math
+    {
+// 1. Create a class with a method that will take in an integer, create a math operation
+// for this integer (addition, subtraction, etc.), then return the answer as an integer:
+// FIRST METHOD
+        public int Reduction(int i)
+        {
+            return i - 2;               // Subtract 2 from the integer parameter taken in.
+        }
+
+// 3a. Add a second method to the class with the same name
+// that will take in a decimal, create a different math operation for it:
+// SECOND METHOD
+        public decimal DecimalDivision(decimal i)
+        {
+            return i/2;               // Divide i by 2, generating a decimal value to use in Program.cs.
+        }
+
+// 5a. Add a third method to the class, with the same name, that will take in a string,
+// convert it to an integer if possible, do a different math operation to it:
+// THIRD METHOD
+        public string StringConversion(string j)
+        {
+            return j;               // 
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodSubmissionAssignment/MainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// 2a. In the Main() method of the console app, instantiate the class and
+            Math math = new Math();
+
+            // 2b. call the one method, passing in an integer. Display the result to the screen.
+            int i = 7;
+            int mathSubtract = math.Reduction(i);
+            Console.WriteLine(mathSubtract);
+            Console.WriteLine("\r");
+
+// 3b. return the (3a, in the Math.cs class file) answer as an integer.
+            int mathDivision = Decimal.ToInt32(math.DecimalDivision(i));    // Var i = 7 and is divided by 2 so = 3.5 but converted to int here so just 3.
+            Console.WriteLine(mathDivision);
+            Console.WriteLine("\r");
+
+// 4a. In the Main() method of the console app, instantiate the class [Why is this asked for; the class is already instantiated.]
+// 4b. call the second method, passing in a decimal. Display the result to the screen.
+            float mathDivisionDecimal = Convert.ToSingle(math.DecimalDivision(i));    // 
+            Console.WriteLine(mathDivisionDecimal);
+            Console.WriteLine("\r");
+
+
+// 5b. return the answer as an integer.
+// 6. In the Main() method of the console app, instantiate the class and call the third method,
+// passing in a string that equates to an integer. Display the result to the screen:
+            string j = "10";                    // Set string variable to value of "10".
+            int stringToInt = int.Parse(j);     // Set int var and use Parse to convert the value of j.
+            Console.WriteLine(stringToInt);     // Write that shiny, sweet goodness.
+            Console.WriteLine("\r");
+
+            Console.ReadLine();
+        }
+    }
+}
+
