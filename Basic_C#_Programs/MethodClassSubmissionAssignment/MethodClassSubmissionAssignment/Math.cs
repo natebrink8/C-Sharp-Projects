87a6b20e32d3702fab26c9cd833856ff7766ee98
nate <147548341+natebrink8@users.noreply.github.com>
2024-04-01 13:14:36 -0700

    METHOD CLASS SUBMISSION ASSIGNMENT code commit.

---
+++ Basic_C#_Programs/MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    class Math
+    {
+// 1. Create a class. In that class, create a void method
+// that takes two integers as parameters.
+// Have the method do a math operation on the first integer
+        // and display the second integer to the screen.
+        public void Method1(int num1, int num2)
+        {
+            num1=num1*10;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodClassSubmissionAssignment/MethodClassSubmissionAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// 2. In the Main() method of the console app, instantiate the class.
+            Math math = new Math();
+
+            int num2 = 10;
+            int num1 = 20;
+
+// 3. Call the method in the class, passing in two numbers.
+            math.Method1(num1, num2);   // PASSING in two numbers.
+            Console.WriteLine("\r");
+
+// 4. Call the method in the class, specifying the parameters by name.
+            math.Method1(num2: 66, num1: 53);
+
+
+            Console.ReadLine();
+        }
+    }
+}
