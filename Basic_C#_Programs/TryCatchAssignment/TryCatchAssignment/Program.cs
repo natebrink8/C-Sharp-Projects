1406aba6606396f6d5667d534855f25fd37c07b9
nate <147548341+natebrink8@users.noreply.github.com>
2024-04-05 14:16:39 -0700

    couple tweaks to this code.

--- Basic_C#_Programs/TryCatchAssignment/TryCatchAssignment/Program.cs
+++ Basic_C#_Programs/TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -13,21 +13,20 @@ namespace TryCatchAssignment
 // 1. Ask the user for their age.
             Console.WriteLine("Please provide your age.");
 
-            // 3. Exceptions must be handled using "try/catch."
+// 3. Exceptions must be handled using "try/catch."
             try
             {
-                string currentYearString = DateTime.Parse(DateTime.Now.ToString()).Year.ToString();
-                int currentYearInt = Convert.ToInt32(currentYearString);
                 int ageInput = Convert.ToInt32(Console.ReadLine());
-                int usersAgeDifference = currentYearInt - ageInput;
                 if (ageInput <= 0)
                 {
                     throw new AgeException();
                 }
+                int currentYear = DateTime.Now.Year;
+                int yearBorn = currentYear - ageInput;
 
 
 // 2. Display the year the user was born.
-                Console.WriteLine("Quick math says you were born in " + usersAgeDifference + "! ;-)");
+                Console.WriteLine("Quick math says you were born in " + yearBorn + "! ;-)");
                 Console.WriteLine("\r");
                 Console.ReadLine();
             }
@@ -39,7 +38,7 @@ namespace TryCatchAssignment
                 Console.ReadLine();
                 return;
             }
-            // 5. Display a general message if an exception was caused by anything else.
+// 5. Display a general message if an exception was caused by anything else.
             catch (Exception)
             {
                 Console.WriteLine("An error occured. Please contact your System Administrator.");
@@ -56,5 +55,3 @@ namespace TryCatchAssignment
         }
     }
 }
-
-// CODE discussed with Andy:
