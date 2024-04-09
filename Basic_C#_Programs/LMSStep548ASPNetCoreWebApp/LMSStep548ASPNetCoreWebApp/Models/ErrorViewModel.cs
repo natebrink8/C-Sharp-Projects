47144df09c8c030cb5c787953e256d60e8035d58
nate <147548341+natebrink8@users.noreply.github.com>
2024-04-08 19:59:16 -0700

    Step 548 CHALLENGE SUBMISSION ASSIGNMENT code commit.

---
+++ Basic_C#_Programs/LMSStep548ASPNetCoreWebApp/LMSStep548ASPNetCoreWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LMSStep548ASPNetCoreWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
